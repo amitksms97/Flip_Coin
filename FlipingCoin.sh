119fe7129f4d99e701fc28bb5673199d726475f7
amtksms97 <amtksms97@gmail.com>
2020-11-05 09:46:59 +0530

    Repeated the flipping of coin 10 times

--- FlipingCoin.sh
+++ FlipingCoin.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-n=$((RANDOM%2))
-if [ $n -eq 0 ]
-then
-	echo "Head is a winner"
-else
-	echo "Tail is a winner"
-fi
+t=0
+h=0
+for(( i=1; i<=10; i++ ))
+do
+	n=$((RANDOM%2))
+	if [ $n -eq 0 ]
+	then
+		echo "Head is a winner"
+		((h++))
+	else
+		echo "Tail is a winner"
+		((t++))
+	fi
+done
+echo "Heads won $h times"
+echo "Tails won $t times"
