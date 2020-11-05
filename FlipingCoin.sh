72abe365340fbe1ffdfa4031e6fa284928c3c3de
amtksms97 <amtksms97@gmail.com>
2020-11-05 10:23:13 +0530

    Completed the whole program with all the given condition

--- FlipingCoin.sh
+++ FlipingCoin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 t=0
 h=0
-while [[ h -ne 21 && t -ne 21 ]]
+while [[ d -le 2 ]]
 do
 	n=$((RANDOM%2))
 	if [ $n -eq 0 ]
@@ -12,19 +12,21 @@ do
 		echo "Tail is a winner"
 		((t++))
 	fi
+	if [[ $h -ge 21 || $t -ge 21 ]]
+	then
+		d=`expr $h - $t`
+		if [ $d -le -1 ]
+		then
+			d=`expr $d \* -1`
+			f=1
+		else
+			f=0
+		fi
+	fi
 done
-d=`expr $h - $t`
-if [ $d -le -1 ]
-then
-	d=`expr $d \* -1`
-	f=1
-else
-	f=0
-fi
 if [ $f -eq 1 ]
 then
-	echo "Tails won $t times"
+	echo "Tails won $t times and Head wins $h times. Overall Tail wins with diffrence of $d"
 else
-	echo  "Heads won $h times"
+	echo "Heads won $h times and Tails win by $t times. Overall Head wins with difffrence of $d"
 fi
-echo "Difference: $d"
