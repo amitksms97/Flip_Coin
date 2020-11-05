f22bad26692843744408209607c8cfcf4e8fcbf4
amtksms97 <amtksms97@gmail.com>
2020-11-05 10:03:22 +0530

    Added: Condition to check which coin wins and by how much

--- FlipingCoin.sh
+++ FlipingCoin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 t=0
 h=0
-for(( i=1; i<=10; i++ ))
+while [[ h -ne 21 && t -ne 21 ]]
 do
 	n=$((RANDOM%2))
 	if [ $n -eq 0 ]
@@ -13,5 +13,18 @@ do
 		((t++))
 	fi
 done
-echo "Heads won $h times"
-echo "Tails won $t times"
+d=`expr $h - $t`
+if [ $d -le -1 ]
+then
+	d=`expr $d \* -1`
+	f=1
+else
+	f=0
+fi
+if [ $f -eq 1 ]
+then
+	echo "Tails won $t times"
+else
+	echo  "Heads won $h times"
+fi
+echo "Difference: $d"
